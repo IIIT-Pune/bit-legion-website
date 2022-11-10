0675d93d10508323506bfd0e0b13785368c75be0
Rushikesh Tote <rushi.tote@gmail.com>
2022-11-10 17:14:14 +0530

    added social links

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Fame from "./components/Fame";
 import NavBar from "./components/NavBar";
 import Archive from "./components/Archive";
 import Team from "./components/Team";
+import { BsInstagram, BsLinkedin, BsTwitter } from "react-icons/bs";
 import "./App.css";
 import homeImage from "./assets/home-image.jpg";
 
@@ -53,6 +54,35 @@ const MainComponent = () => {
         ICPC Amritapuri regionals.
       </div>
       <Team />
+      <div className="flex flex-row justify-center items-center">
+        <div className="text-pink-squid text-xl px-2 font-medium">
+          Find us on:{" "}
+        </div>
+        <button
+          className="px-3"
+          onClick={() => {
+            window.open("https://www.instagram.com/bit.iiitp", "_blank");
+          }}
+        >
+          <BsInstagram className="text-pink-squid" size={30} />
+        </button>
+        <button
+          className="px-3`"
+          onClick={() => {
+            window.open("https://twitter.com/bit_iiitp", "_blank");
+          }}
+        >
+          <BsTwitter className="text-pink-squid" size={30} />
+        </button>
+        <button
+          className="px-3"
+          onClick={() => {
+            window.open("https://www.linkedin.com/company/bitlegion", "_blank");
+          }}
+        >
+          <BsLinkedin className="text-pink-squid" size={30} />
+        </button>
+      </div>
     </div>
   );
 };
